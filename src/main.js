54343a37057980a6e9d964c80afe69d5dac92241
Noah Cohn <noahc42@gmail.com>
2026-04-28 01:15:05 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -374,7 +374,7 @@ async function init() {
 
     // Billiard Lamp (async OBJ load)
     const lampResult = await createLamp(scene);
-    lampResult.group.position.set(0, 13.5, 0);
+    lampResult.group.position.set(0, 8.0, 0);
     registerInteractiveObject(lampResult.group, lampResult.toggle);
     lampData = lampResult;
     updateRegistry.register('lamp', lampResult.update);
